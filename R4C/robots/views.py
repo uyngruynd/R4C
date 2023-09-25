65cef85b720aa8455de71f9feacf205f09fabb00
Aleksei <fedotov.alexm@gmail.com>
2023-09-25 18:50:00 +0200

    task #1 completed

--- R4C/robots/views.py
+++ R4C/robots/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from robots.models import Robot
+from django.core.exceptions import PermissionDenied
+from django.views.decorators.csrf import csrf_exempt
+from http import HTTPStatus
+from datetime import datetime
 
-# Create your views here.
+
+def is_datetime_valid(value: str) -> bool:
+    try:
+        datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
+        return True
+    except:
+        return False
+
+
+def get_serial(model: str, version: str) -> str:
+    return f'{model}-{version}'
+
+
+@csrf_exempt
+def create(request):
+    if not request.POST:
+        raise PermissionDenied
+
+    if not all(key in request.POST for key in ('model', 'version', 'created')):
+        return HttpResponse('Проверьте наличие обязательных полей в запросе!',
+                            status=HTTPStatus.BAD_REQUEST)
+
+    model = request.POST['model'].strip()
+    version = request.POST['version'].strip()
+    created = request.POST['created'].strip()
+
+    if not is_datetime_valid(created):
+        return HttpResponse('Неверный формат даты!',
+                            status=HTTPStatus.BAD_REQUEST)
+
+    existing_models = Robot.objects.values_list('model', flat=True)
+
+    if model not in existing_models:
+        return HttpResponse(f'Модель {model} не зарегистрирована!',
+                            status=HTTPStatus.BAD_REQUEST)
+
+    serial = get_serial(model, version)
+
+    is_robot_exists = Robot.objects.filter(serial=serial,
+                                           created=created).exists()
+
+    if is_robot_exists:
+        return HttpResponse(
+            f'Робот {serial} уже зарегистрирован на дату {created}!',
+            status=HTTPStatus.BAD_REQUEST)
+
+    robot = Robot(model=model,
+                  version=version,
+                  created=created,
+                  serial=serial)
+    robot.save()
+
+    return HttpResponse('Новый робот успешно зарегистрирован!')
